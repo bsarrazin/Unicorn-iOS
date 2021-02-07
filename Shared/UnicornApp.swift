d6fb11e2477b00432d3b4cb75a4c6f2447680527
Ben Sarrazin <b@srz.io>
2021-02-07 16:34:49 -0500

    Initial app

---
+++ Shared/UnicornApp.swift
@@ -0,0 +1,12 @@
+import Logging
+import Networking
+import SwiftUI
+
+@main
+struct UnicornApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
